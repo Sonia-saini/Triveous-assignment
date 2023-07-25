ac8c870698b9c0d6249d4e743dd6fd75c6aef2d4
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-07-25 12:44:19 +0530

    basic structure

---
+++ assignment/src/Router/Allroute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+function Allroute() {
+  return (
+    <div>
+        <Routes>
+            {/* <Route path="/" element={<A} */}
+        </Routes>
+    </div>
+  )
+}
+
+export default Allroute

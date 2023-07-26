a9e0e2c257e3eb02de88b94132f3d46887b94fa8
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-07-26 16:32:30 +0530

    added favourite functionality

--- assignment/src/Pages/SingleNews.js
+++ assignment/src/Pages/SingleNews.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   chakra,
@@ -17,6 +17,7 @@ import {
   List,
   ListItem,
   Link,
+  useToast,
 } from "@chakra-ui/react";
 import { MdLocalShipping } from "react-icons/md";
 import { useParams } from "react-router-dom";
@@ -24,10 +25,40 @@ import { FaRegHeart } from "react-icons/fa";
 import { BsHeartFill } from "react-icons/bs";
 function SingleNews() {
   const { name } = useParams();
+  const toast = useToast();
   const [like, setLike] = useState(false);
   let allnews = JSON.parse(localStorage.getItem("news")) || [];
   let detailnews = allnews?.find((el) => el.source.name === name);
   console.log(detailnews, "details");
+  let addliked = JSON.parse(localStorage.getItem("fav")) || [];
+  useEffect(() => {
+    let alreadyadded = addliked.find(
+      (el) => el.source.name === detailnews.source.name
+    );
+    if (like && !alreadyadded) {
+      addliked.push(detailnews);
+      localStorage.setItem("fav", JSON.stringify(addliked));
+      toast({
+        title: "Successfully Added to Favourite",
+        description: "Added to Favourite",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    } else if (alreadyadded) {
+      let removed = addliked.filter(
+        (el) => el.source.name !== detailnews.source.name
+      );
+      localStorage.setItem("fav", JSON.stringify(removed));
+      toast({
+        title: "Successfully Removed to Favourite",
+        description: "Removed to Favourite",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }, [like]);
   return (
     <Container maxW={"7xl"}>
       <Button
--- assignment/src/Router/Allroute.js
+++ assignment/src/Router/Allroute.js
@@ -5,15 +5,38 @@ import Login from "../Pages/Login";
 import SingleNews from "../Pages/SingleNews";
 import Register from "../Pages/Registeration";
 import Authentication from "../component/Authentication";
+import Favourite from "../Pages/Favourite";
 
 function Allroute() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Authentication><Allnews /></Authentication>} />
+        <Route
+          path="/"
+          element={
+            <Authentication>
+              <Allnews />
+            </Authentication>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/detailnews/:name" element={<Authentication><SingleNews /></Authentication>} />
+        <Route
+          path="/detailnews/:name"
+          element={
+            <Authentication>
+              <SingleNews />
+            </Authentication>
+          }
+        />
+        <Route
+          path="/favouritenews"
+          element={
+            <Authentication>
+              <Favourite />
+            </Authentication>
+          }
+        />
       </Routes>
     </div>
   );
